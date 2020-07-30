d1dabbeaec4bdb39b5281793d77bec0b83491422
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-07-29 19:08:26 -0600

    created code for the schema.sql

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
+CREATE TABLE employee(
+	 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     first_name VARCHAR (30) NOT NULL,
+     last_name VARCHAR (30) NOT NULL,
+     role_id INT NOT NULL,
+     manager_id INT NOT NULL
+);
+
+SELECT * FROM employee;
+
+CREATE TABLE role (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL,
+    department_id INT 
+);
+
+SELECT * FROM role; 
+
+CREATE TABLE department (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30) NOT NULL
+);
+
+SELECT * FROM department; 
---
+++ server.js
@@ -0,0 +1,39 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// setting up our database connection (but not connecting yet!)
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "employeetracker_db"
+});
+
+// connecting to our database
+connection.connect(err => {
+    if (err) throw err;
+
+    // and initially displaying the user menu
+    start();//build a function on this 
+});
+
+//using this code!!
+// const start = async () => {
+//     // pausing to get the user's requested action
+//     const { action } = await inquirer.prompt({
+//         name: "action",
+//         type: "list",
+//         message: "Would you like to [POST] an auction or [BID] on an auction?",
+//         choices: ["POST", "BID", "EXIT"]
+//     });
+
+//     // ...after the await above finishes, calling the appropriate function
+//     switch (action) {
+//         case "POST":
+//             return postAuction();
+//         case "BID":
+//             return bidAuction();
+//         default:
+//             connection.end();
+//     }
