c285e1e7bdff3b4585661aff9745a76c8f3a98b3
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-07-29 20:15:58 -0600

    created second switch case for when createEmployee() is selected, sicne there are three seperate tables. Need to allow the user to enter in data specific to each table

--- server.js
+++ server.js
@@ -41,79 +41,131 @@ const userMenu = () => {
 };
 
 const createEmployee= () => {
-    connection.query("SELECT * FROM department", (err, items) => {
-        if (err) throw err; 
-            inquirer.prompt([
-                {//first is the department (name of department)
-                    name:"name",
-                    message: "What is the name of the Department?"
-                },
-            ]).then(({name}))
-        })
-    };
-
-
-
-
-
-        {//this starts the role table, with values of title,salary and department_id 
-            name:"title",
-            message: "What is the title of the employee?"
-        },
+    inquirer.prompt([//two stitch cases
         {
-            name: "salary",
-            message: "What is the currently salary of the employee?",
-            validate: function (val){
-                //used to verify that a number was entered in
-                if (isNaN(val)){
-                    return `${val} is not a number.`
-                } 
-               return true;
-            }
-        },
-        {
-            name: "department_id",
-            message: "What is the department ID of this employee?",
-            validate: function (val){
-                //used to verify that a number was entered in
-                if (isNaN(val)){
-                    return `${val} is not a number.`
-                } 
-               return true;
-            }
-        },
-        {//starts the employee table values of first_name,last_name, role_id, manager_id
-            name: "first_name",
-            message: "What is the employees first name?"
-        },
-        {
-            name: "last_name",
-            message: "What is the employees last name?"
-        },
-        {
-            name: "role_id",
-            message: "What is the role ID?",
-            validate: function (val){
-                //used to verify that a number was entered in
-                if (isNaN(val)){
-                    return `${val} is not a number.`
-                } 
-               return true;
-            }
-        },
-        {
-            name: "manager_id",
-            message: "What is the manager ID? If not manager, leave blank.",
-            validate: function (val){
-                //used to verify that a number was entered in
-                if (isNaN(val)){
-                    return `${val} is not a number.`
-                } 
-               return true;
-            }
+            type: "list",
+           name: "action",
+           message: "Which table would you like to work on?",
+           choices:["Department", "Role","Employee","Exit"] 
         }
-    ])
-}
+    ]).then(({ action})=>{
+        switch (action){
+            case "Department":
+                departmentInfo();
+                break;
+            case "Role":
+                roleInfo();
+                break;
+            case "Employee":
+                employeeInfo();
+                break;
+            default:
+                connection.end();
+        }
+    })
+};
+
+const departmentInfo = () => {
+    inquirer.prompt([
+        {//first is the department (name of department)
+            name:"name",
+            message: "What is the name of the Department?"
+        }
+    ]).then(({name}) => {
+        connection.query("INSERT INTO department SET ?", {name},
+        (err, result) => {
+            if (err) throw err;
+            console.log (`Successfully add ${name} into the department table.`),
+            createEmployee();
+        }
+      )
+    })
+};
+
+
+
+    //!These questions will go in each of their own functions created from the switch above 
+    
+    //  {//this starts the role table, with values of title,salary and department_id 
+    //         name:"title",
+    //         message: "What is the title of the employee?"
+    //     },
+    //     {
+    //         name: "salary",
+    //         message: "What is the currently salary of the employee?",
+    //         validate: function (val){
+    //             //used to verify that a number was entered in
+    //             if (isNaN(val)){
+    //                 return `${val} is not a number.`
+    //             } 
+    //            return true;
+    //         }
+    //     },
+    //     {
+    //         name: "department_id",
+    //         message: "What is the department ID of this employee?",
+    //         validate: function (val){
+    //             //used to verify that a number was entered in
+    //             if (isNaN(val)){
+    //                 return `${val} is not a number.`
+    //             } 
+    //            return true;
+    //         }
+    //     },
+    //     {//starts the employee table values of first_name,last_name, role_id, manager_id
+    //         name: "first_name",
+    //         message: "What is the employees first name?"
+    //     },
+    //     {
+    //         name: "last_name",
+    //         message: "What is the employees last name?"
+    //     },
+    //     {
+    //         name: "role_id",
+    //         message: "What is the role ID?",
+    //         validate: function (val){
+    //             //used to verify that a number was entered in
+    //             if (isNaN(val)){
+    //                 return `${val} is not a number.`
+    //             } 
+    //            return true;
+    //         }
+    //     },
+    //     {
+    //         name: "manager_id",
+    //         message: "What is the manager ID? If not manager, leave blank.",
+    //         validate: function (val){
+    //             //used to verify that a number was entered in
+    //             if (isNaN(val)){
+    //                 return `${val} is not a number.`
+    //             } 
+    //            return true;
+    //         }
+    //     }
+    
+    //     function updateEmployee() {
+    //         console.log("Updating your changes.\n");
+    //         //*!when doing this function: remember you have three data tables and need to specify where the update is going (department, role or employee) and what is being updated!
+    //         var query = connection.query(
+    //           "UPDATE employees SET ? WHERE ?",
+    //           [
+    //             {
+    //               quantity: 100
+    //             },
+    //             {
+    //               flavor: "Rocky Road"
+    //             }
+    //           ],
+    //           function(err, res) {
+    //             if (err) throw err;
+    //             console.log(res.affectedRows + " products updated!\n");
+                
+    //           }
+    //         );
+          
+    //         // logs the actual query being run
+    //         console.log(query.sql);
+    //       }
 
 
 // connecting to our database
