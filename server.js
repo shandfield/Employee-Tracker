a72bd6a13b5c1d7429cb52fff7a134dfbf5f58ae
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-07-29 19:12:51 -0600

    took out code for an ajax example, planning on using .this code

--- server.js
+++ server.js
@@ -11,25 +11,6 @@ const connection = mysql.createConnection({
 });
 
 
-//using this code!!
-// const start = async () => {
-//     // pausing to get the user's requested action
-//     const { action } = await inquirer.prompt({
-//         name: "action",
-//         type: "list",
-//         message: "Would you like to [POST] an auction or [BID] on an auction?",
-//         choices: ["POST", "BID", "EXIT"]
-//     });
-
-//     // ...after the await above finishes, calling the appropriate function
-//     switch (action) {
-//         case "POST":
-//             return postAuction();
-//         case "BID":
-//             return bidAuction();
-//         default:
-//             connection.end();
-//     }
 
 // connecting to our database
 connection.connect((err)=> {
