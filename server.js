dde6fee2f716f6aa20517bdab98c9d8eca1c73f5
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-07-29 19:33:11 -0600

    updaing the code to start using inquirer to build out the department table

--- server.js
+++ server.js
@@ -10,9 +10,19 @@ const connection = mysql.createConnection({
     database: "employeetracker_db"
 });
 
+//building the command line that allows a user to make data entries regarding their staff:
+const userMenu = () => {
+    inquirer.prompt([
+        {//first is the department (name of department)
+            name:"name"
+        }
+    ])
+}
 
 
 // connecting to our database
 connection.connect((err)=> {
     if (err) throw err;
-    console.log(`Now connected to MySQL at thread ${connection.threadID}!`)
+    console.log(`Now connected to MySQL at thread ${connection.threadID}!`);
+    userMenu();
+});
